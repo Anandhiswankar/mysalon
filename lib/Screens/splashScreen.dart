8dd7314ad940a67e28c5929f348477d81b99d044
anandhiswankar <anandhiswankar3@gmail.com>
2024-05-10 23:24:00 +0530

    init 2

---
+++ lib/Screens/splashScreen.dart
@@ -0,0 +1,65 @@
+import 'package:lottie/lottie.dart';
+import 'package:flutter/material.dart';
+import 'package:mysalon/elements/color.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class SpalshScreen extends StatefulWidget {
+  const SpalshScreen({super.key});
+
+  @override
+  State<SpalshScreen> createState() => _SpalshScreenState();
+}
+
+class _SpalshScreenState extends State<SpalshScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: colorBackground,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              decoration: const BoxDecoration(boxShadow: [
+                BoxShadow(
+                    color: Color.fromARGB(87, 0, 0, 0),
+                    offset: Offset(0, 2),
+                    blurRadius: 15,
+                    spreadRadius: 5)
+              ], color: Colors.white, shape: BoxShape.circle),
+              width: 200,
+              height: 200,
+              child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Lottie.asset("assets/json/startlogo.json")),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            AnimatedTextKit(
+              animatedTexts: [
+                FadeAnimatedText("Ziggly",
+                    textStyle:
+                        GoogleFonts.aclonica(fontSize: 45, color: Colors.white),
+                    duration: Duration(seconds: 2)),
+              ],
+              totalRepeatCount: 2,
+              displayFullTextOnTap: false,
+              stopPauseOnTap: false,
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
