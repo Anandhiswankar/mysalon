c981c13a56a2e3894c87950e81f28985db7f697a
Divyesh Khamele <divyesh@pythonmate.com>
2024-05-18 00:40:29 +0530

    mainUpdate

--- lib/Screens/authscreens/loginscreen.dart
+++ lib/Screens/authscreens/loginscreen.dart
@@ -9,7 +9,7 @@ import 'package:mysalon/elements/namecircle.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mysalon/elements/fullviewbtn.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:mysalon/services/firestore/checkuserindb.dart';
 
--- lib/services/firestore/checkuserindb.dart
+++ lib/services/firestore/checkuserindb.dart
@@ -1,44 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:mysalon/elements/alertbox.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<bool> checkUserindb(BuildContext context) async {
-  // var cp = FirebaseAuth.instance.currentUser;
+  var cp = FirebaseAuth.instance.currentUser;
 
-  // alertBox(context);
-  // var cu = await FirebaseFirestore.instance
-  //     .collection("users")
-  //     .where("uid", isEqualTo: cp!.uid.toString())
-  //     .get()
-  //     .then((value) {
-  //   if (value.docs.isEmpty) {
-  //     print("User not in database");
+  alertBox(context);
+  var cu = await FirebaseFirestore.instance
+      .collection("users")
+      .where("uid", isEqualTo: cp!.uid.toString())
+      .get()
+      .then((value) {
+    if (value.docs.isEmpty) {
+      print("User not in database");
 
-  //     // Navigator.of(context)
-  //     //     .pushReplacement(MaterialPageRoute(builder: (context) {
-  //     //   return const newAccount();
-  //     // }));
-  //     return true;
-  //   } else {
-  //     print("User Found in database");
+      // Navigator.of(context)
+      //     .pushReplacement(MaterialPageRoute(builder: (context) {
+      //   return const newAccount();
+      // }));
+      return true;
+    } else {
+      print("User Found in database");
 
-  //     var data = value.docs.first.data() as dynamic;
+      var data = value.docs.first.data() as dynamic;
 
-  //     if (data["role"].toString().toLowerCase() == "user" && data["isUser"]) {
-  //       // replaceScreen(context, HomePageUser());
-  //     } else {
-  //       if (data["role"].toString().toLowerCase() == "salon" &&
-  //           data["isSalon"]) {
-  //         // replaceScreen(context, SalonHomePage());
-  //       } else if (data["role"].toString().toLowerCase() == "admin" &&
-  //           data["isAdmin"]) {
-  //         // nextScreen(context, AdminHome());
-  //       }
-  //     }
+      if (data["role"].toString().toLowerCase() == "user" && data["isUser"]) {
+        // replaceScreen(context, HomePageUser());
+      } else {
+        if (data["role"].toString().toLowerCase() == "salon" &&
+            data["isSalon"]) {
+          // replaceScreen(context, SalonHomePage());
+        } else if (data["role"].toString().toLowerCase() == "admin" &&
+            data["isAdmin"]) {
+          // nextScreen(context, AdminHome());
+        }
+      }
 
-  //     return false;
-  //   }
-  // });
+      return false;
+    }
+  });
   return false;
 }
