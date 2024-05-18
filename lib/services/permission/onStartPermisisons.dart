c52ac69f0b517de99aab3d70f1332e633194f195
anandhiswankar <anandhiswankar3@gmail.com>
2024-05-18 23:53:16 +0530

    perissionUpdate

--- lib/services/permission/onStartPermisisons.dart
+++ lib/services/permission/onStartPermisisons.dart
@@ -1,17 +1,38 @@
 import 'package:permission_handler/permission_handler.dart';
 
 onStartPermission() async {
+  bool isAllTrue = false;
   var locationPermission = await Permission.locationWhenInUse.isGranted;
 
   if (locationPermission) {
-    return true;
+    isAllTrue = true;
+  } else {
+    isAllTrue = false;
   }
 
   var permissionRequest = await Permission.locationWhenInUse.request();
 
   if (permissionRequest.isGranted) {
-    return true;
+    isAllTrue = true;
+  } else {
+    isAllTrue = false;
   }
 
-  return false;
+  locationPermission = await Permission.photos.isGranted;
+
+  if (locationPermission) {
+    isAllTrue = true;
+  } else {
+    isAllTrue = false;
+  }
+
+  permissionRequest = await Permission.photos.request();
+
+  if (permissionRequest.isGranted) {
+    isAllTrue = true;
+  } else {
+    isAllTrue = false;
+  }
+
+  return isAllTrue;
 }
